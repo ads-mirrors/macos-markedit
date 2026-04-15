MarkEdit-app/MarkEdit

Fix recently introduced build errors

MarkEditMac/Sources/Editor/Controllers/EditorViewController+UI.swift
@@ -83,15 +83,17 @@ extension EditorViewController {
 
   @available(macOS 15.1, *)
   func updateWritingTools(isActive: Bool) {
-    let performUpdate = {
-      // Ignore beforeInput handling to work around undo stack issues
-      self.bridge.writingTools.setActive(
-        isActive: isActive,
-        reselect: MarkEditWritingTools.shouldReselect(with: MarkEditWritingTools.requestedTool)
-      )
+    let performUpdate: @Sendable () -> Void = {
+      Task { @MainActor in
+        // Ignore beforeInput handling to work around undo stack issues
+        self.bridge.writingTools.setActive(
+          isActive: isActive,
+          reselect: MarkEditWritingTools.shouldReselect(with: MarkEditWritingTools.requestedTool)
+        )
 
-      // Invisible rendering doesn't work well with WritingTools, temporarily disable it for now
-      self.setInvisiblesBehavior(behavior: isActive ? .never : AppPreferences.Editor.invisiblesBehavior)
+        // Invisible rendering doesn't work well with WritingTools, temporarily disable it for now
+        self.setInvisiblesBehavior(behavior: isActive ? .never : AppPreferences.Editor.invisiblesBehavior)
+      }
     }
 
     if isActive {

ads-mirrors/macos-markedit

MarkEditMac/Sources/Editor/Controllers/EditorViewController+UI.swift
@@ -185,15 +185,17 @@ extension EditorViewController {
 
   @available(macOS 15.1, *)
   func updateWritingTools(isActive: Bool) {
-    let performUpdate = {
-      // Work around undo stack and selection range issues
-      self.bridge.writingTools.setActive(
-        isActive: isActive,
-        reselect: AppWritingTools.shouldReselect(with: AppWritingTools.requestedTool)
-      )
+    let performUpdate: @Sendable () -> Void = {
+      Task { @MainActor in
+        // Work around undo stack and selection range issues
+        self.bridge.writingTools.setActive(
+          isActive: isActive,
+          reselect: AppWritingTools.shouldReselect(with: AppWritingTools.requestedTool)
+        )
 
-      // Invisible rendering doesn't work well with Writing Tools, temporarily disable it for now
-      self.setInvisiblesBehavior(behavior: isActive ? .never : AppPreferences.Editor.invisiblesBehavior)
+        // Invisible rendering doesn't work well with Writing Tools, temporarily disable it for now
+        self.setInvisiblesBehavior(behavior: isActive ? .never : AppPreferences.Editor.invisiblesBehavior)
+      }
     }
 
     if isActive {