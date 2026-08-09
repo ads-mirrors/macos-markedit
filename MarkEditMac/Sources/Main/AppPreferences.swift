MarkEdit-app/MarkEdit

SWIFT_STRICT_CONCURRENCY = complete

MarkEditMac/Sources/Main/AppPreferences.swift
@@ -13,6 +13,7 @@ import FontPicker
 /**
  UserDefaults wrapper with handy getters and setters.
  */
+@MainActor
 enum AppPreferences {
   enum General {
     @Storage(key: "general.appearance", defaultValue: .system)
@@ -51,18 +52,14 @@ enum AppPreferences {
     @Storage(key: "editor.light-theme", defaultValue: AppTheme.GitHubLight.editorTheme)
     static var lightTheme: String {
       didSet {
-        Task { @MainActor in
-          AppTheme.current.updateAppearance(animateChanges: true)
-        }
+        AppTheme.current.updateAppearance(animateChanges: true)
       }
     }
 
     @Storage(key: "editor.dark-theme", defaultValue: AppTheme.GitHubDark.editorTheme)
     static var darkTheme: String {
       didSet {
-        Task { @MainActor in
-          AppTheme.current.updateAppearance(animateChanges: true)
-        }
+        AppTheme.current.updateAppearance(animateChanges: true)
       }
     }
 
@@ -170,10 +167,8 @@ enum AppPreferences {
     @Storage(key: "assistant.inline-predictions", defaultValue: true)
     static var inlinePredictions: Bool {
       didSet {
-        Task { @MainActor in
-          NSSpellChecker.InlineCompletion.spellCheckerEnabled = inlinePredictions
-          performUpdates { $0.setInlinePredictions(enabled: inlinePredictions) }
-        }
+        NSSpellChecker.InlineCompletion.spellCheckerEnabled = inlinePredictions
+        performUpdates { $0.setInlinePredictions(enabled: inlinePredictions) }
       }
     }
 
@@ -282,6 +277,7 @@ enum Appearance: Codable {
   case light
   case dark
 
+  @MainActor
   func resolved(with appearance: NSAppearance = NSApp.effectiveAppearance) -> NSAppearance? {
     switch self {
     case .system:
@@ -368,14 +364,13 @@ extension NSWindow.TabbingMode: Codable {}
 
 private extension AppPreferences {
   static func performUpdates(action: @escaping (EditorViewController) -> Void) {
-    Task { @MainActor in
-      for editor in EditorReusePool.shared.viewControllers() {
-        action(editor)
-      }
+    for editor in EditorReusePool.shared.viewControllers() {
+      action(editor)
     }
   }
 }
 
+@MainActor
 @propertyWrapper
 struct Storage<T: Codable> {
   private let key: String

ads-mirrors/macos-markedit

MarkEditMac/Sources/Main/AppPreferences.swift
@@ -15,6 +15,7 @@ import FontPicker
 /**
  UserDefaults wrapper with handy getters and setters.
  */
+@MainActor
 enum AppPreferences {
   enum General {
     @Storage(key: "general.appearance", defaultValue: .system)
@@ -61,18 +62,14 @@ enum AppPreferences {
     @Storage(key: "editor.light-theme", defaultValue: AppTheme.GitHubLight.editorTheme)
     static var lightTheme: String {
       didSet {
-        Task { @MainActor in
-          AppTheme.current.updateAppearance(animateChanges: true)
-        }
+        AppTheme.current.updateAppearance(animateChanges: true)
       }
     }
 
     @Storage(key: "editor.dark-theme", defaultValue: AppTheme.GitHubDark.editorTheme)
     static var darkTheme: String {
       didSet {
-        Task { @MainActor in
-          AppTheme.current.updateAppearance(animateChanges: true)
-        }
+        AppTheme.current.updateAppearance(animateChanges: true)
       }
     }
 
@@ -180,10 +177,8 @@ enum AppPreferences {
     @Storage(key: "assistant.inline-predictions", defaultValue: true)
     static var inlinePredictions: Bool {
       didSet {
-        Task { @MainActor in
-          NSSpellChecker.InlineCompletion.spellCheckerEnabled = inlinePredictions
-          performUpdates { $0.setInlinePredictions(enabled: inlinePredictions) }
-        }
+        NSSpellChecker.InlineCompletion.spellCheckerEnabled = inlinePredictions
+        performUpdates { $0.setInlinePredictions(enabled: inlinePredictions) }
       }
     }
 
@@ -363,6 +358,7 @@ enum Appearance: String, CompactCodableEnum {
   case light
   case dark
 
+  @MainActor
   func resolved(with appearance: NSAppearance = NSApp.effectiveAppearance) -> NSAppearance? {
     switch self {
     case .system:
@@ -450,14 +446,13 @@ extension NSWindow.TabbingMode: @retroactive Codable {}
 
 private extension AppPreferences {
   static func performUpdates(action: @escaping (EditorViewController) -> Void) {
-    Task { @MainActor in
-      for editor in EditorPreloader.shared.viewControllers() {
-        action(editor)
-      }
+    for editor in EditorPreloader.shared.viewControllers() {
+      action(editor)
     }
   }
 }
 
+@MainActor
 @propertyWrapper
 struct Storage<T: Codable> {
   private let key: String